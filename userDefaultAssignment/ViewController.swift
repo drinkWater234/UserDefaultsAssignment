86e4f20cc47d814a31ab5c0b0b69b3ed7e34c3f4
William <hsiang.chuang@enhanceit.us>
2022-12-29 16:34:24 -0500

    Initial Commit

---
+++ userDefaultAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  userDefaultAssignment
+//
+//  Created by Consultant on 12/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
